6f40629bd75d666e9abae7194b459beb3d21732a
Julien Levasseur <julien.levasseur@free.fr>
2023-09-05 12:42:24 -0400

    extend list command with consul profiles if consul config is provided

--- cmd/list.go
+++ cmd/list.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"strings"
 
+	"github.com/julienlevasseur/profiler/pkg/consul"
 	"github.com/julienlevasseur/profiler/pkg/profile"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -23,24 +25,43 @@ var listCmd = &cobra.Command{
 			"*.yaml",
 		)
 
-		files = append(files, yamlFiles...)
+		if viper.GetString("consulAddress") != "" || viper.GetString("ssmRegion") != "" {
+			fmt.Println("[Local Profiles]")
+		}
 
-		for _, file := range files {
-			fmt.Println(
-				strings.Split(
-					strings.Split(
-						file,
-						fmt.Sprintf(
-							"%s/.",
-							viper.GetString("profilerFolder"),
-						),
-					)[1], ".y", // The separator here is '.y' to support both .yml and .yaml files
-				)[0],
-			)
+		files = append(files, yamlFiles...)
+		listLocalProfiles(files)
+
+		// Checking for Consul config:
+		if viper.GetString("consulAddress") != "" {
+			consulProfiles, err := consul.ListProfiles()
+			if err != nil {
+				log.Printf("Error while listing Consul profiles: %w", err)
+			}
+			fmt.Println("\n[Consul Remote Profiles]")
+			for _, profile := range consulProfiles {
+				fmt.Println(profile)
+			}
 		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(listCmd)
+}
+
+func listLocalProfiles(files []string) {
+	for _, file := range files {
+		fmt.Println(
+			strings.Split(
+				strings.Split(
+					file,
+					fmt.Sprintf(
+						"%s/.",
+						viper.GetString("profilerFolder"),
+					),
+				)[1], ".y", // The separator here is '.y' to support both .yml and .yaml files
+			)[0],
+		)
+	}
 }
